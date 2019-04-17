b79db312ce77db024e17959a70f788c5e43159ed
Simon Howroyd <howroydlsu@gmail.com>
2019-04-17 16:01:05 +0100

    Basic MQTT broker example

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,47 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include "uMQTTBroker.h"
-
-uMQTTBroker myBroker;
-
-void startWiFiClient();
-void startWiFiAP();
-
+#include "myWifi.h"
 
 /*
  * Your WiFi config here
  */
-char ssid[] = "ssid";      // your network SSID (name)
-char pass[] = "password"; // your network password
+char* ssid = wifiHome.ssid;     // your network SSID (name)
+char* pass = wifiHome.pass;     // your network password
 
+void startWiFiClient();
+void startWiFiAP();
+bool WiFiAP = false; // Do yo want the ESP as AP?
 
+/*
+ * Custom broker class with overwritten callback functions
+ */
+class myMQTTBroker : public uMQTTBroker
+{
+public:
+  virtual bool onConnect(IPAddress addr, uint16_t client_count)
+  {
+    Serial.println(addr.toString() + " connected");
+    return true;
+  }
+
+  virtual bool onAuth(String username, String password)
+  {
+    Serial.println("Username/Password: " + username + "/" + password);
+    return true;
+  }
+
+  virtual void onData(String topic, const char *data, uint32_t length)
+  {
+    char data_str[length + 1];
+    os_memcpy(data_str, data, length);
+    data_str[length] = '\0';
+
+    Serial.println("received topic '" + topic + "' with data '" + (String)data_str + "'");
+  }
+};
+
+myMQTTBroker myBroker;
 
 void setup()
 {
@@ -22,20 +49,32 @@ void setup()
   Serial.println();
   Serial.println();
 
-  // Connect to a WiFi network
-  startWiFiClient();
-
-  // Or start the ESP as AP
-//startWiFiAP();
+  // Start WiFi
+  if (WiFiAP)
+    startWiFiAP();
+  else
+    startWiFiClient();
 
   // Start the broker
   Serial.println("Starting MQTT broker");
   myBroker.init();
+
+  /*
+ * Subscribe to anything
+ */
+  myBroker.subscribe("#");
 }
 
+int counter = 0;
+
 void loop()
-{   
-  // do anything here
+{
+  /*
+ * Publish the counter value as String
+ */
+  myBroker.publish("broker/counter", (String)counter++);
+
+  // wait a second
   delay(1000);
 }
 
@@ -44,15 +83,16 @@ void loop()
  */
 void startWiFiClient()
 {
-  Serial.println("Connecting to "+(String)ssid);
+  Serial.println("Connecting to " + (String)ssid);
   WiFi.begin(ssid, pass);
-  
-  while (WiFi.status() != WL_CONNECTED) {
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
   Serial.println("");
-  
+
   Serial.println("WiFi connected");
   Serial.println("IP address: " + WiFi.localIP().toString());
 }
